7cfddf2e70335b45213e9dab9d45dbc0c30744d7
phannedroid <phannedroid@github.com>
2022-02-23 21:46:48 +0000

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -5,16 +5,13 @@ PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/jobs')
-
 def open_connection():
     connection = getattr(g, '_connection', None)
 
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
 
-    connection.row_factory = sqlite3.row
+    connection.row_factory = sqlite3.Row
     return connection
 
 def execute_sql(sql, values=(), commit=False, single=False):
@@ -36,5 +33,7 @@ def close_connection(exception):
     if connection is not None:
         connection.close()
 
+@app.route('/')
+@app.route('/jobs')
 def jobs():
     return render_template('index.html')
